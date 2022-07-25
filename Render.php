802c49aa3ee85b4a33a2ea94b6840da2af5be759
Rafael Alexandre <rafael.alexandre.1996.ra@gmail.com>
2022-07-25 16:47:38 -0300

    Arrumando Render.php

--- Render.php
+++ Render.php
@@ -1,17 +1,17 @@
 <?php
 
 function renderView($titulo, $conteudo, $renderHeader = true, $renderFooter = true) {
-  $page = file_get_contents(__DIR__ . '\pages\page.html');
+  $page = file_get_contents(__DIR__ . '/pages/page.html');
   $page = str_replace('{{titulo}}', $titulo, $page);
 
-  $header = ($renderHeader == true) ? file_get_contents(__DIR__ . '\pages\header.html') : '';
+  $header = ($renderHeader == true) ? file_get_contents(__DIR__ . '/pages/header.html') : '';
   $page = str_replace('{{header}}', $header, $page);
 
-  $view_patch = str_replace('{{main}}', $conteudo, '\pages\{{main}}.html');
+  $view_patch = str_replace('{{main}}', $conteudo, '/pages/{{main}}.html');
   $main = file_get_contents(__DIR__ . $view_patch);
   $page = str_replace('{{main}}', $main, $page);
 
-  $footer = ($renderFooter == true) ? file_get_contents(__DIR__ . '\pages\footer.html') : '';
+  $footer = ($renderFooter == true) ? file_get_contents(__DIR__ . '/pages/footer.html') : '';
   $page = str_replace('{{footer}}', $footer, $page);
 
   $page = str_replace('{{a:linkedin}}', 'https://www.linkedin.com/in/rafael-alexandre-766793bb/', $page);
